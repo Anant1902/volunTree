b3b750dcfe5431dc1d5cf21d59ab6162c768997b
evedaktyl <evedaktyl@gmail.com>
2024-02-08 22:50:26 +0800

    Rebase

--- app/index.js
+++ app/index.js
@@ -3,7 +3,10 @@ import MainContainerAdmin from '../navigation/MainContainerAdmin';
 import MainContainerUser from '../navigation/MainContainerUser';
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 188bf76 (Update file structure)
 export default function App() {
   const page = "admi";
   return page === "admin" ? <MainContainerAdmin /> : <MainContainerUser />;
