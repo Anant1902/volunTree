5bcdf6070c7bb4b346376cadc4e4cebc0a0b3c56
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-02-08 15:21:53 +0800

    Add carousel feature

---
+++ slides.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id:'1',
+        title:'Quick & Easy payments',
+        description:'grow your bsinies',
+        image: require('./assets/images/bunny.png'),
+    },
+    {
+        id:'2',
+        title:'dogs',
+        description:'grow your bsinies',
+        image: require('./assets/images/doggo.png'),
+    },
+    {
+        id:'3',
+        title:'Quick & Easy payments',
+        description:'grow your bsinies',
+        image: require('./assets/images/bunny.png'),
+    },
+    {
+        id:'4',
+        title:'dogs',
+        description:'grow your bsinies',
+        image: require('./assets/images/doggo.png'),
+    },
+
+]
