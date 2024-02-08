2a544a9f5975902952b37d46266cbc761ccad1d8
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-02-08 21:23:41 +0800

    Clean up code

--- app/index.js
+++ app/index.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import MainContainerAdmin from '../navigation/MainContainerAdmin';
 import MainContainerUser from '../navigation/MainContainerUser';
-import Onboarding from '../components/Onboarding';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
 
 
 
 export default function App() {
-  const page = "admin";
+  const page = "admi";
   return page === "admin" ? <MainContainerAdmin /> : <MainContainerUser />;
   
 }
--- navigation/screens/AttendanceScreen.js
+++ navigation/screens/AttendanceScreen.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import Onboarding from '../../components/Onboarding';
 
 export default function AttendanceScreen({ navigation }) {
     return (
